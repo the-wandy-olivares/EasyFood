d1da766abc346a4fd5beaae512d7cbe546649b02
Wandy Olivares <user.email>
2025-03-10 06:24:13 -0400

    Mejorar la vista de punto de venta (POS): actualizar estilos de categorías, eliminar vista de elementos y agregar nueva plantilla para platos.

--- EasyFood/Company/views.py
+++ EasyFood/Company/views.py
@@ -1061,12 +1061,15 @@ class Facturacion(TemplateView):
 class POS(TemplateView):
       template_name = "company/pos/pos.html"
 
-      help = "Ayuda"
       def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
-       
+            context['list_categorias'] = models.Category.objects.filter(is_active=True)
+            categoria_id = self.request.GET.get('categoria')
+            if categoria_id:
+                  context['platos'] = models.Plato.objects.filter(category_id=categoria_id)
+            else:
+                  context['platos'] = models.Plato.objects.all()[:10]
             return context
-      
 # Hola mundo
 """ 
             Hola
